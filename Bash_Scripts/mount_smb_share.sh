ca69d7088d3eec164e993467715444545906d9b9
Javel Palmer <37094748+j4v3l@users.noreply.github.com>
2023-10-01 23:40:05 -0400

    Update mount_smb_share.sh
    Second revision

--- Bash_Scripts/mount_smb_share.sh
+++ Bash_Scripts/mount_smb_share.sh
@@ -5,7 +5,8 @@ read_input_secure() {
   prompt=$1
   var_name=$2
 
-  read -rsp "$prompt: " $var_name
+  echo -n "$prompt: "
+  IFS= read -rs "$var_name"
   echo
 }
 
@@ -20,38 +21,52 @@ install_cifs_utils() {
   fi
 }
 
-# Function to check if sambaclient is installed and list SMB drives
-list_smb_drives() {
-  if ! dpkg -l samba-client &>/dev/null; then
-    echo "samba-client package not found. Please install it to list SMB drives."
-    exit 1
+# Function to install smbclient package if not installed
+install_smbclient() {
+  if ! dpkg -l smbclient &>/dev/null; then
+    echo "smbclient package not found. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y smbclient
   else
-    echo "Listing SMB drives..."
-    sambaclient -L "$1" -U "$2%$3"
+    echo "smbclient package is already installed."
   fi
+}
+
+# Function to check if smbclient is installed and list SMB drives
+list_smb_drives() {
+  if ! dpkg -l smbclient &>/dev/null; then
+    echo "smbclient package not found. Installing..."
+    install_smbclient
+  fi
+
+  echo "Listing SMB drives..."
+  smbclient -L "$1" -U "$2%$3" || exit 1
 }
 
 # Function to mount SMB share
 mount_smb_share() {
   local smb_username=$1
-  local smb_password=$2
-  local remote_host=$3
-  local shared_dir=$4
-  local mount_dir=$5
+  local smb_password
+  local remote_host=$2
+  local shared_dir=$3
+  local mount_dir=$4
 
-  # Store the credentials in .creds file
-  echo "username=${smb_username}" >"$mount_dir/.creds"
-  echo "password=${smb_password}" >>"$mount_dir/.creds"
-  chmod 400 "$mount_dir/.creds"
+  # Read SMB password securely
+  read_input_secure "Enter your SMB password" smb_password
+
+  # Store the credentials in /opt/.creds file
+  echo "username=${smb_username}" | sudo tee "/opt/.creds" > /dev/null
+  echo "password=${smb_password}" | sudo tee -a "/opt/.creds" > /dev/null
+  sudo chmod 400 "/opt/.creds"
 
   # Create the mount directory
-  mkdir -p "$mount_dir"
+  sudo mkdir -p "$mount_dir"
 
   # Mount the SMB share
-  sudo mount -t cifs -o rw,vers=3.1.1,credentials="$mount_dir/.creds" "//${remote_host}${shared_dir}" "$mount_dir"
+  sudo mount -t cifs -o rw,vers=3.1.1,credentials="/opt/.creds" "//${remote_host}${shared_dir}" "$mount_dir"
 
   # Automount on system reboot
-  echo -e "//${remote_host}${shared_dir} ${mount_dir} cifs vers=3.1.1,credentials=${mount_dir}/.creds 0 0" | sudo tee -a /etc/fstab
+  echo -e "//${remote_host}${shared_dir} ${mount_dir} cifs vers=3.1.1,credentials=/opt/.creds 0 0" | sudo tee -a /etc/fstab
 
   echo "SMB share mounted successfully for: ${mount_dir}"
 }
@@ -59,10 +74,12 @@ mount_smb_share() {
 # Check and install cifs-utils if needed
 install_cifs_utils
 
-# Check and list SMB drives if sambaclient is installed
+# Check and install smbclient if needed
+install_smbclient
+
+# Check and list SMB drives if smbclient is installed
 read -p "Enter the remote host IP address or hostname: " remote_host
 read_input_secure "Enter your SMB username" smb_username
-read_input_secure "Enter your SMB password" smb_password
 
 list_smb_drives "$remote_host" "$smb_username" "$smb_password"
 
@@ -75,5 +92,5 @@ for ((i = 1; i <= max_drives; i++)); do
   read -p "Enter the mount directory path for SMB drive ${i}: " mount_dir
 
   # Call the mount_smb_share function
-  mount_smb_share "$smb_username" "$smb_password" "$remote_host" "$shared_dir" "$mount_dir"
+  mount_smb_share "$smb_username" "$remote_host" "$shared_dir" "$mount_dir"
 done
