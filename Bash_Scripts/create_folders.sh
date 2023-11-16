e361f734607977bda5bb55ddec93e9447028f6fb
Javel Palmer <37094748+j4v3l@users.noreply.github.com>
2023-11-16 11:45:18 -0500

    Update create_folders.sh
    Major refactoring

--- Bash_Scripts/create_folders.sh
+++ Bash_Scripts/create_folders.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+cat << "EOF"
+ __  __ _  ______ ___ ____  
+|  \/  | |/ /  _ \_ _|  _ \ 
+| |\/| | ' /| | | | || |_) |
+| |  | | . \| |_| | ||  _ < 
+|_|  |_|_|\_\____/___|_| \_\
+                            
+-----------------------------------------------------
+by J4V3L (2023-11)
+-----------------------------------------------------
+EOF
+echo "-----------------------------------------------------"
 
-read -p "Enter the number of folders you want to create: " num_folders
 read -p "Enter the path where you want to create the folders: " folder_path
 
 # Check if the folder_path exists and is a directory
@@ -12,19 +23,42 @@ fi
 # Create an array to store folder names
 folders=()
 
-# Create the folders
-for ((i = 1; i <= num_folders; i++)); do
-  read -p "Enter the name for folder $i: " folder_name
+echo "Enter folder names (type 'done' to finish):"
+
+while true; do
+  read -p "Enter folder name: " folder_name
+
+  # Sentinel value to break the loop
+  if [[ $folder_name == "done" ]]; then
+    break
+  fi
+
+  # Check for empty folder name
+  if [[ -z $folder_name ]]; then
+    echo "Folder name cannot be empty. Please try again."
+    continue
+  fi
+
   folder="${folder_path}/${folder_name}"
-  sudo mkdir -p "$folder"
-  sudo chown -R $(whoami) "$folder" # Change ownership to the current user
-  echo "Folder '$folder_name' created at '$folder_path'."
+
+  # Check if the folder already exists
+  if [ -d "$folder" ]; then
+    echo "Folder '$folder_name' already exists at '$folder_path'."
+    continue
+  fi
+
+  mkdir -p "$folder" && echo "Folder '$folder_name' created at '$folder_path'." || echo "Failed to create '$folder_name'."
   folders+=("$folder_name")
 done
 
-echo "All $num_folders folders created successfully."
+# Check if any folders were created
+if [ ${#folders[@]} -eq 0 ]; then
+  echo "No folders were created."
+else
+  echo "Folders created successfully."
 
-# Display the created folders in a table and column format
-echo -e "\nCreated folders:"
-echo -e "-----------------"
-echo "${folders[@]}" | column
+  # Display the created folders in a table and column format
+  echo -e "\nCreated folders:"
+  echo -e "-----------------"
+  echo "${folders[@]}" | column
+fi
